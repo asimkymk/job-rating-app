632bb9d524ae5326fc455793eaac2a67e004e911
Asım Kaymak <kaymakasm@gmail.com>
2023-05-02 02:12:11 +0300

    db created

---
+++ backend/db_worker.py
@@ -0,0 +1,202 @@
+import sqlite3
+from sqlite3 import Error
+
+def create_connection():
+    conn = None
+    try:
+        conn = sqlite3.connect('./backend/db/all_data.db')
+        return conn
+    
+    except Error as e:
+        print(e)
+
+    return conn
+
+def close_connection(con):
+    try:
+        con.close()
+    except:
+        return False
+    return True
+
+def create_users_table(conn):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("""
+            CREATE TABLE users(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT UNIQUE NOT NULL,
+                password TEXT NOT NULL,
+                email TEXT UNIQUE,
+                name TEXT NOT NULL,
+                surname TEXT NOT NULL,
+                birtYear INTEGER NOT NULL
+            );
+        """)
+        print("users table created successfully")
+    except:
+        return False
+    return True
+
+def create_jobs_table(conn):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS jobs(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                employee_id INTEGER NOT NULL,
+                employer_id INTEGER NOT NULL,
+                title TEXT NOT NULL,
+                description TEXT,
+                status TEXT NOT NULL,
+                createDate TEXT NOT NULL,
+                lastEditDate TEXT,
+                FOREIGN KEY (employee_id) REFERENCES users (id),
+                FOREIGN KEY (employer_id) REFERENCES users (id)
+            );
+        """)
+        conn.commit()
+        print("jobs table created successfully")
+    except:
+        return False
+    return True
+
+def create_feedbacks_table(conn):
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS feedbacks(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            job_id INTEGER NOT NULL,
+            employer_id INTEGER NOT NULL,
+            rate INTEGER NOT NULL,
+            comment TEXT NOT NULL,
+            createdDate TEXT NOT NULL,
+            FOREIGN KEY (job_id) REFERENCES jobs (id),
+            FOREIGN KEY (employer_id) REFERENCES users (id)
+        );
+    """)
+    conn.commit()
+    print("feedbacks table created successfully")
+
+
+def add_user(conn, username, password, email, name, surname, birth_year):
+    cursor = conn.cursor()
+    try:
+        cursor.execute("""
+            INSERT INTO User(username, password, email, name, surname, birthYear)
+            VALUES(?, ?, ?, ?, ?, ?)
+        """, (username, password, email, name, surname, birth_year))
+        conn.commit()
+        print("User added successfully")
+    except Error as e:
+        print(e)
+        return False
+    return True
+
+def add_job(conn, employee_id, employer_id, title, description, status, create_date):
+    cursor = conn.cursor()
+    try:
+        cursor.execute("""
+            INSERT INTO jobs(employee_id, employer_id, title, description, status, createDate, lastEditDate)
+            VALUES(?, ?, ?, ?, ?, ?)
+        """, (employee_id, employer_id, title, description, status, create_date))
+        conn.commit()
+        print("Job added successfully")
+    except Error as e:
+        print(e)
+        return False
+    return True
+
+def add_job(conn, employee_id, employer_id, title, description, status, create_date, last_edit_date):
+    cursor = conn.cursor()
+    try:
+        cursor.execute("""
+            INSERT INTO jobs(employee_id, employer_id, title, description, status, createDate, lastEditDate)
+            VALUES(?, ?, ?, ?, ?, ?, ?)
+        """, (employee_id, employer_id, title, description, status, create_date, last_edit_date))
+        conn.commit()
+        print("Job added successfully")
+    except Error as e:
+        print(e)
+        return False
+    return True
+
+def add_feedback(conn, job_id, employer_id, rate, comment, createdDate):
+    cursor = conn.cursor()
+    try:
+        cursor.execute("""
+            INSERT INTO feedbacks(job_id, employer_id, rate, comment, createdDate) 
+            VALUES(?, ?, ?, ?, ?);
+        """, (job_id, employer_id, rate, comment, createdDate))
+        conn.commit()
+        print("Feedback added successfully")
+    except:
+        return False
+    return True
+
+def get_user(conn, username, password):
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT * FROM users WHERE username = ? AND password = ?;
+    """, (username, password))
+    rows = cursor.fet
+    for row in rows:
+        print(row)
+
+def get_job(conn, user_id):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT * FROM jobs WHERE employer_id = ? OR employee_id = ?;
+        """, (user_id,user_id,))
+        user = cursor.one()
+        return user
+    except:
+        return False
+    
+def get_feedback(conn, employer_id):
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT * FROM feedbacks WHERE employer_id = ?;
+    """, (employer_id,))
+    rows = cursor.fetchall()
+    if rows:
+        return rows
+    else:
+        return []
+    
+def delete_user(conn, user_id):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("""
+            DELETE FROM users WHERE id = ?;
+        """, (user_id,))
+        conn.commit()
+        print("User deleted successfully")
+    except:
+        return False
+    return True
+
+def delete_jobs(conn, job_id):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("""
+            DELETE FROM jobs WHERE id = ?;
+        """, (job_id,))
+        conn.commit()
+        print("Job deleted successfully")
+    except:
+        return False
+    return True
+
+def delete_feedbacks(conn, feedback_id):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("""
+            DELETE FROM feedbacks WHERE id = ?;
+        """, (feedback_id,))
+        conn.commit()
+        print("Feedback deleted successfully")
+    except:
+        return False
+    return True

84f1a018db78a2eb9ecd86054cae2abcab30747f
Asım Kaymak <kaymakasm@gmail.com>
2023-05-03 02:13:00 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -34,8 +34,17 @@ def login():
         hex_dig = hash_object.hexdigest()
         user = get_user(conn,request.json['username'],hex_dig)
         close_connection(conn)
-        return jsonify(dict(user)), 200
-    except:
+        dict_user = {
+            "id":user[0],
+            "username":user[1],
+            "email":user[3],
+            "name":user[4],
+            "surname":user[5],
+            "birthYear":user[6]
+        }
+        return jsonify((dict_user)), 200
+    except Exception as e:
+        print(e)
         return jsonify({'error': 'Database error!'}), 400
     
 
@@ -43,14 +52,14 @@ def login():
 def signup():
     try:
 
-        if not request.json or not 'username' in request.json or not 'password' in request.json or not 'email' in request.json or not 'name' in request.json or not 'surname' in request.json:
+        if not request.json or not 'username' in request.json or not 'password' in request.json or not 'email' in request.json or not 'name' in request.json or not 'surname' in request.json or not "birthYear" in request.json:
             return jsonify({'error': 'The reuqired field is required'}), 400
         conn = create_connection()
         password = request.json['password']
         text_encoded = password.encode('utf-8')
         hash_object = hashlib.md5(text_encoded)
         hex_dig = hash_object.hexdigest()
-        if add_user(conn,request.json['username'],hex_dig,request.json['email'],request.json['name'],request.json['surname'],request.json['birth_year']):
+        if add_user(conn,request.json['username'],hex_dig,request.json['email'],request.json['name'],request.json['surname'],request.json['birthYear']):
             return jsonify({'success': 'Signup successful!'}), 200
         else:
             return jsonify({'error': 'Signup error!'}), 400

55861fa7b01fca42b7288695310918ef1d2b7770
Asım Kaymak <kaymakasm@gmail.com>
2023-05-03 18:53:11 +0300

    backend functtions initial version done

--- backend/app.py
+++ backend/app.py
@@ -3,9 +3,14 @@ from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS #comment this on deployment
 from db_worker import *
 import hashlib
+from flask_jwt import JWT, jwt_required, current_identity
+from models import *
+
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 CORS(app) #comment this on deployment
 api = Api(app)
+app.config['SECRET_KEY'] = 'super-secret'
+
 #BACKEND
 @app.route('/createdatabase', methods=['GET'])
 def createdatabase():
@@ -26,7 +31,7 @@ def login():
     try:
 
         if not request.json or not 'username' in request.json or not 'password' in request.json:
-            return jsonify({'error': 'The username and password field is required'}), 400
+            return jsonify({'error': 'The username and password field are required'}), 400
         conn = create_connection()
         password = request.json['password']
         text_encoded = password.encode('utf-8')
@@ -53,7 +58,7 @@ def signup():
     try:
 
         if not request.json or not 'username' in request.json or not 'password' in request.json or not 'email' in request.json or not 'name' in request.json or not 'surname' in request.json or not "birthYear" in request.json:
-            return jsonify({'error': 'The reuqired field is required'}), 400
+            return jsonify({'error': 'Some fields are required'}), 400
         conn = create_connection()
         password = request.json['password']
         text_encoded = password.encode('utf-8')
@@ -65,3 +70,50 @@ def signup():
             return jsonify({'error': 'Signup error!'}), 400
     except:
         return jsonify({'error': 'Database error!'}), 400
+
+@app.route('/job/add', methods=['POST'])
+def addjob():
+    try:
+
+        if not request.json or not 'employee_id' in request.json or not 'employer_id' in request.json or not 'title' in request.json or not 'description' in request.json or not 'createDate' in request.json:
+            return jsonify({'error': 'Some fields are required'}), 400
+        conn = create_connection()
+        if add_job(conn,request.json['employee_id'],request.json['employer_id'],request.json['title'],request.json['description'],request.json['createDate']):
+            return jsonify({'success': 'Job created succesfuly!'}), 200
+        else:
+            return jsonify({'error': 'Creation error!'}), 400
+    except:
+        return jsonify({'error': 'Database error!'}), 400
+
+@app.route('/job/<int:employer_id>', methods=['GET'])
+def getjob(employer_id):
+    try:
+        conn = create_connection()
+        rows = get_job(conn,employer_id)
+        return jsonify(rows), 200
+    except:
+        return jsonify({'error': 'Database error!'}), 400
+
+@app.route('/feedback/add', methods=['POST'])
+def addfeedback():
+    try:
+
+        if not request.json or not 'job_id' in request.json or not 'employer_id' in request.json or not 'rate' in request.json or not 'comment' in request.json or not 'createDate' in request.json:
+            return jsonify({'error': 'Some fields are required'}), 400
+        conn = create_connection()
+        if add_feedback(conn,request.json['job_id'],request.json['employer_id'],request.json['rate'],request.json['comment'],request.json['createDate']):
+            return jsonify({'success': 'Feedback created succesfuly!'}), 200
+        else:
+            return jsonify({'error': 'Creation error!'}), 400
+    except:
+        return jsonify({'error': 'Database error!'}), 400
+
+@app.route('/feedback/<int:job_id>', methods=['GET'])
+def  getfeecback(job_id):
+    try:
+        conn = create_connection()
+        rows = get_feedback(conn,job_id)
+        return jsonify(rows), 200
+    except:
+        return jsonify({'error': 'Database error!'}), 400
+
